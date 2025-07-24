a3efc87a06cf37b995bade4ef28f929954ef42ce
Dhivakar <dhivakarp305@gmail.com>
2025-07-24 12:43:54 +0530

    Fetch:New Folder

---
+++ src/Dhivakar/Dhivakar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Dhivakar = () => {
+  return (
+    <div>
+      <h1>Hello Dhivakar</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor temporibus nemo nobis enim, distinctio qui corrupti, accusantium illo labore adipisci eaque deserunt quasi esse architecto nihil reprehenderit perspiciatis! Soluta laudantium temporibus doloribus, neque dignissimos deserunt facere provident dolor sint necessitatibus.</p>
+    </div>
+  )
+}
+
+export default Dhivakar
